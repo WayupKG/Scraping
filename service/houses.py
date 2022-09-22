48baf2641cb4d522ea3c58f70b897009556ed7fb
WayupKG <adikgk232323@gmail.com>
2022-09-22 15:38:56 +0600

    Добавлено асинхронная версия в ветке asyncio

--- service/houses.py
+++ service/houses.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import asyncio
+import aiohttp
 
 from bs4 import BeautifulSoup
 
@@ -9,38 +11,41 @@ from service import save_house_db
 from service import save_house_google_sheets
 
 
+house_data = []
 
-def get_house(response):
-    soup = BeautifulSoup(response, 'lxml')
-    houses = soup.find("main").find_all("div", class_="search-item")
-    page_data = []
-    for item in houses:
-        title = item.find("a", class_="title").text.strip(),
-        price = item.find("div", class_="price").text.strip(),
-        date = item.find("div", class_="location").find('span', class_="date-posted").text.strip(),
-        image_ = item.find("img").get("data-src")
-        image = image_ if image_ is not None else not_image
-        page_data.append([title[0], price[0], date[0], image])
-    return page_data
-
-
-def get_response_page(page: int):
+async def get_page_data(session, page):
     full_url = f"{url}page-{page}/{url_end}"
-    response = requests.get(full_url, headers=headers)
-    response_page = re.findall(r'\d{1}/|\d{2}/', response.url)
-    current_page = int(response_page[0].replace('/', '')) if response_page else 1
-    context = {'response': response.text, 'page': current_page}
-    return context
+    async with session.get(url=full_url, headers=headers) as response:
+        response_text = await response.text()
+
+        # response_page = re.findall(r'\d{1}/|\d{2}/', response.url)
+        # current_page = int(response_page[0].replace('/', '')) if response_page else 1
+        # context = {'response': response.text, 'page': current_page}
+        # response_page = get_response_page(i)
+        # page_data = get_house(response_page['response'])
+        # data.extend(page_data)
+        # print(f"\nПолучено данные страницы = {i}")
+                
+        soup = BeautifulSoup(response_text, 'lxml')
+        houses = soup.find("main").find_all("div", class_="search-item")
+        for item in houses:
+            title = item.find("a", class_="title").text.strip(),
+            price = item.find("div", class_="price").text.strip(),
+            date = item.find("div", class_="location").find('span', class_="date-posted").text.strip(),
+            image_ = item.find("img").get("data-src")
+            image = image_ if image_ is not None else not_image
+            house_data.append([title[0], price[0], date[0], image])
+        print(f"[INFO] Обработано страницы {page}")
 
 
-def get_page_houses(start_page: int, end_page: int):
-    data = []
-    for i in range(start_page, end_page + 1):
-        response_page = get_response_page(i)
-        page_data = get_house(response_page['response'])
-        data.extend(page_data)
-        print(f"\nПолучено данные страницы = {i}")
-    return data
+
+async def get_page_houses(start_page: int, end_page: int):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for page in range(start_page, end_page + 1):
+            task = asyncio.create_task(get_page_data(session, page))
+            tasks.append(task)
+        await asyncio.gather(*tasks)            
 
 
 def main():
@@ -52,12 +57,12 @@ def main():
                 print("\nУкажите диапазон страницы")
                 start_page, end_page = input("начало страницы - "), input("конец страницы - ")
                 if start_page.isdigit() and end_page.isdigit():
-                    data_houses = get_page_houses(int(start_page), int(end_page))
+                    asyncio.run(get_page_houses(int(start_page), int(end_page)))
                     match int(answer_user):
                         case 1:
-                            save_house_db.save(data_houses)
+                            save_house_db.save(house_data)
                         case 2:
-                            save_house_google_sheets.save(data_houses)
+                            save_house_google_sheets.save(house_data)
                     break
                 print("\nУкажите корректные данные")
             break
