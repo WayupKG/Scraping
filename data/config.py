e523e2074731c6ac02adcd3cf2e3e65b62baa07b
WayupKG <adikgk232323@gmail.com>
2022-09-29 20:54:55 +0600

    Добавлено поддержка база данных PostgreSQL

--- data/config.py
+++ data/config.py
@@ -1,11 +1,15 @@
+import os
+
 from environs import Env
 
 env = Env()
 env.read_env()
 
+# Ссылка сайта
 url = "https://www.kijiji.ca/b-apartments-condos/city-of-toronto/"
 url_end = 'c37l1700273'
 
+# Ссылка нужен для не найденных изображении
 not_image = "https://afs.googleusercontent.com/kijiji-ca/csa-image1-large.png"
 
 headers = {
@@ -13,8 +17,22 @@ headers = {
     "User-Agent":  env.str("USER_AGENT")
 }
 
-
 # Файл, полученный в Google Developer Console
 CREDENTIALS_FILE = "credentials.json"
+
 # ID Google Sheets документа (можно взять из его URL)
 spreadsheet_id = env.str("SHEET_ID")
+
+# Настройки базы данных
+DATABASE = env.str("DATABASE")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", 'db.sqlite3')
+DATABASE_USER = os.environ.get("DATABASE_USER", "user")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "password")
+DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.environ.get("DATABASE_PORT", "5432")
+
+if DATABASE == 'postgresql':
+    DATABASE_ENGINE = f"{DATABASE}+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}"\
+                      f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+else:
+    DATABASE_ENGINE = f"sqlite:///{DATABASE_NAME}"
--- models/base.py
+++ models/base.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///sqlite3.db')
+from data.config import DATABASE_ENGINE
+
+# создать движок и начать взаимодействовать с базами данных
+engine = create_engine(DATABASE_ENGINE)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
